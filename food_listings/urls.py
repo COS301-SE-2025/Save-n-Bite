f4b92e8eb547df22f58a4d7f1ea6c62cbe9e86d2
MarcoGeral <u23584565@tuks.co.za>
2025-05-25 12:08:47 +0200

    updated urls

--- food_listings/urls.py
+++ food_listings/urls.py
@@ -14,13 +14,4 @@ urlpatterns = [
     # Customer browsing endpoints
     path('food-listings/', views.browse_food_listings, name='browse_listings'),
     path('food-listings/<uuid:listing_id>/', views.get_food_listing_details, name='listing_details'),
-    
-    # Cart endpoints
-    path('cart/', views.get_cart_items, name='get_cart'),
-    path('cart/add/', views.add_to_cart, name='add_to_cart'),
-    path('cart/remove/', views.remove_from_cart, name='remove_from_cart'),
-    path('cart/checkout/', views.checkout_cart, name='checkout'),
-    
-    # Order endpoints
-    path('orders/<uuid:order_id>/pickup/', views.get_pickup_details, name='pickup_details'),
 ]
