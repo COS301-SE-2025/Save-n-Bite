76182d0178f55020052819a2a626bac6af1eba3d
MarcoGeral <u23584565@tuks.co.za>
2025-06-23 13:56:30 +0200

    updated main urls

--- save-n-bite-backend/backend/urls.py
+++ save-n-bite-backend/backend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/', include('food_listings.urls')),
     path('cart/',include('interactions.urls')),
     path('api/', include('notifications.urls')),
+    path('api/scheduling/', include('scheduling.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Serve media files during development
