d3120cca778727d714d3fa6e3a26da713d9254d9
Capleton11 <u22554875@tuks.co.za>
2025-07-15 13:45:28 +0200

    Created forgot Password api

---
+++ save-n-bite-frontend/src/services/ForgotPasswordAPI.js
@@ -0,0 +1,72 @@
+import { apiClient } from './FoodAPI.js';
+
+
+const ForgotPassword = {
+
+
+  ResetPassword: async(user_email) =>{
+
+    try {
+        
+        const response = await apiClient.post(`/auth/forgot-password/`, {email : user_email});
+
+        return{
+            success: true,
+            error: null,
+            data: response.data
+        }
+
+    } catch (error) {
+
+        return{
+            error: true,
+            success: null,
+            error: error.response?.data?.message || error.message || "Could not reset your password"
+        }
+        
+    }
+
+  },
+
+  LoginEnhanced: async(user_email, user_password) => {
+
+try {
+
+    const response = await apiClient.post(`/auth/login-enhanced/`, {
+        email: user_email,
+        password: user_password
+    })
+
+
+    return{
+
+        success: true,
+        error: null,
+        data: response.data
+    }
+    
+} catch (error) {
+
+    return{
+
+        success: null,
+        data: null,
+        error: true,
+        error: error.response?.data?.message || error.message || "Failed to reset the password"
+    }
+    
+}
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
+export default ForgotPassword
