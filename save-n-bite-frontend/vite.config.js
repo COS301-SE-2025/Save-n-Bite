088a08d319023915d585c9d4232787e2b76711c4
Capleton11 <u22554875@tuks.co.za>
2025-07-20 21:04:34 +0200

    updated vite again

--- save-n-bite-frontend/vite.config.js
+++ save-n-bite-frontend/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'savenbiteportal-f5ggcpczf5f2f8b4.southafricanorth-01.azurewebsites.net',
+      '.azurewebsites.net',
+      'localhost'
+    ]
   }
 })
