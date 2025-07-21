87ad32d8f2ac2c20341f703cb8b052cd3a45749e
Capleton11 <u22554875@tuks.co.za>
2025-07-21 17:34:46 +0200

    upadated settings.py

--- save-n-bite-backend/backend/settings.py
+++ save-n-bite-backend/backend/settings.py
@@ -31,10 +31,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG") == "false"
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 
