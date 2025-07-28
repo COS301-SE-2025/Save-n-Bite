7a867c26aa6b0040cef41c1f71a2915e5229d248
Capleton11 <u22554875@tuks.co.za>
2025-07-28 09:53:11 +0200

    changes

--- save-n-bite-frontend/src/services/FoodAPI.js
+++ save-n-bite-frontend/src/services/FoodAPI.js
@@ -1,7 +1,7 @@
 // src/services/foodAPI.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://savenbiteservice-hzghg8gcgddtcfg7.southafricanorth-01.azurewebsites.net';
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
