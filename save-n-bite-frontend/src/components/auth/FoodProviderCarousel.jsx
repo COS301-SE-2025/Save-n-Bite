c4a538d8216b0729e8c37c298430429cab34d9a0
Capleton11 <u22554875@tuks.co.za>
2025-07-31 20:25:21 +0200

    Update FoodProviderCarousel.jsx

--- save-n-bite-frontend/src/components/auth/FoodProviderCarousel.jsx
+++ save-n-bite-frontend/src/components/auth/FoodProviderCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChevronLeft, ChevronRight, Star } from 'lucide-react';
 import { Link } from 'react-router-dom' 
 
