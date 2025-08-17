ae48920342e12a1fca662986976a3b6049a66bc2
Sabrina-Gabriel Freeman <sgfreeman24@gmail.com>
2025-08-17 10:54:52 +0200

    fixed casing error and set up azure

--- save-n-bite-backend/backend/settings.py
+++ save-n-bite-backend/backend/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv("DEBUG") == "false"
-DEBUG = os.getenv("DEBUG", "True").lower() == "true"
+DEBUG = os.getenv("DEBUG") == "false"
+#DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,savenbiteservice-hzghg8gcgddtcfg7.southafricanorth-01.azurewebsites.net").split(",")
