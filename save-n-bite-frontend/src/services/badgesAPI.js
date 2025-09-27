58a085b6901aed06289178503677393a92cd6d52
MarcoGeral <u23584565@tuks.co.za>
2025-09-27 16:16:29 +0200

    updated urls

--- save-n-bite-frontend/src/services/badgesAPI.js
+++ save-n-bite-frontend/src/services/badgesAPI.js
@@ -1,6 +1,7 @@
 // src/services/badgesAPI.js
 
-const API_BASE_URL = 'http://localhost:8000';
+//const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://savenbiteservice-hzghg8gcgddtcfg7.southafricanorth-01.azurewebsites.net';
 
 class BadgesAPI {
   constructor() {
